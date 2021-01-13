b724008eb9e925bf3ae746eee274b3d2bc1868c0
unknown <aloha.hat.2508@gmail.com>
2021-01-13 16:46:28 +0700

    Thêm JWT Login vào

---
+++ src/main/java/com/example/demo/model/UserPrinciple.java
@@ -0,0 +1,65 @@
+package com.example.demo.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserPrinciple implements UserDetails {
+    private Long userId;
+
+    private String username;
+
+    private String password;
+
+
+    public UserPrinciple(Long id, String username, String password) {
+        this.userId = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public static UserPrinciple build(User user){
+
+        return new UserPrinciple(
+                user.getUserId(),
+                user.getUsername(),
+                user.getPassword()
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
