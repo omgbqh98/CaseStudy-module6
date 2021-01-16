acef4c9c066b892462786f011d76fbe6e96b9093
omgbqh98 <69282402+omgbqh98@users.noreply.github.com>
2021-01-16 17:32:07 +0700

    sua file

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -41,7 +41,7 @@ public class UserController {
         Optional<User> user = userService.findById(id);
         return userService.findById(user.get().getUserId());
     }
-
+// cap nhat tin user
     @PutMapping("/update")
     public ResponseEntity<User> updateUser(@RequestBody User user) {
         User user1 = userService.findByUsername(user.getUsername());
@@ -61,14 +61,14 @@ public class UserController {
         return new ResponseEntity<>(houses,HttpStatus.OK);
     }
 
-    // Cập nhật thông tin user
-    @PutMapping("/{username}")
-    public ResponseEntity<User> updateProfile(@PathVariable String username, @RequestBody User user) {
-        User userOptional = this.userService.findByUsername(username);
-        user.setUsername(userOptional.getUsername());
-        user.setPassword(userOptional.getPassword());
-        user.setUserId(userOptional.getUserId());
-        userService.save(user);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+//    // Cập nhật thông tin user
+//    @PutMapping("/{username}")
+//    public ResponseEntity<User> updateProfile(@PathVariable String username, @RequestBody User user) {
+//        User userOptional = this.userService.findByUsername(username);
+//        user.setUsername(userOptional.getUsername());
+//        user.setPassword(userOptional.getPassword());
+//        user.setUserId(userOptional.getUserId());
+//        userService.save(user);
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
 }
