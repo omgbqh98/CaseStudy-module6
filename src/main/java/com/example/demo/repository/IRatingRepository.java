b410baa17cf20e5a57266872381dfe72f8cefe9a
unknown <aloha.hat.2508@gmail.com>
2021-01-19 18:03:09 +0700

    thêm 2 query tìm người book, rated vào IRatingRepository

--- src/main/java/com/example/demo/repository/IRatingRepository.java
+++ src/main/java/com/example/demo/repository/IRatingRepository.java
@@ -2,10 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Booking;
 import com.example.demo.model.Rating;
+import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.jws.soap.SOAPBinding;
 
 @Repository
 public interface IRatingRepository extends JpaRepository<Rating, Long> {
@@ -27,4 +30,16 @@ public interface IRatingRepository extends JpaRepository<Rating, Long> {
     // Lấy tất cả rating, sắp xếp theo parentId
     @Query(value = "select * from rating where house_id_house_id = :id order by parent_id desc;", nativeQuery = true)
     Iterable<Rating> findAllByHouseIdParentIdDesc(@Param("id") Long id);
+
+
+    // Lấy tất cả những người đã book nhà
+    @Query(value = "SELECT  user_id_user_id from booking where house_id_house_id = :id AND check_out >= CURDATE();", nativeQuery = true)
+    Iterable<User> findBookedUserByHouse(@Param("id") Long id);
+
+    // Lấy tất cả những người đã book + checkout + đã rate
+    @Query(value="SELECT  rating.user_id_user_id from rating " +
+            "INNER JOIN booking ON rating.user_id_user_id = booking.user_id_user_id " +
+            "where booking.house_id_house_id = :id AND booking.check_out >= CURDATE() " +
+            "AND rating.parent_id = rating.rating_id group by rating.user_id_user_id;", nativeQuery = true)
+    Iterable<User> findCheckedOutAndRatedUserByHouse(@Param("id") Long id);
 }
