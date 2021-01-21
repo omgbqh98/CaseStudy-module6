c55d46c67f1c51c332a708bff48c5961a9936932
unknown <aloha.hat.2508@gmail.com>
2021-01-21 10:31:19 +0700

    thêm Query lấy tất cả những booking đã checkout nhưng chưa rate trong 3 tháng gần nhất vào booking Service

--- src/main/java/com/example/demo/repository/IBookingRepository.java
+++ src/main/java/com/example/demo/repository/IBookingRepository.java
@@ -18,4 +18,8 @@ public interface IBookingRepository extends JpaRepository<Booking,Long> {
     // Lấy tất cả những booking đã checkout của một người
     @Query(value = "select * from booking where check_out <= CURDATE() AND user_id_user_id = :id;", nativeQuery = true)
     Booking findCheckedOutBookingByUser(@Param("id") Long id);
+
+    // Lấy tất cả những booking đã checkout nhưng chưa rate trong 3 tháng gần nhất
+    @Query(value = "SELECT * FROM booking where check_out < curdate() AND is_rated = 0 AND datediff (curdate(), check_out) < 90 AND user_id_user_id = :id;", nativeQuery = true)
+    Iterable<Booking> findAllBookingNotRatedInThreeMonths (@Param("id") Long id);
 }
